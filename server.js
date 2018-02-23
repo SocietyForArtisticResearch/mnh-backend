e172f4c62a1f181bfc3dbb2b9fc4c81e6df31210
Luc Döbereiner <luc@computer.localdomain>
2018-02-23 17:06:31 +0100

    servin json, media list

--- server.js
+++ server.js
@@ -26,26 +26,41 @@ app.post('/import', function(req, res) {
 	(type == "application/msword") ||
 	(type == "application/vnd.oasis.opendocument.text")) {
 	correctType = true;
-	console.log("correct type");
     }
     
     if (correctType) {
 	// Use the mv() method to place the file somewhere on your server
 	convertFile.mv('files/' + convertFile.name, function(err) {
-	    if (err) {
+	    if (err) 
 		return res.status(500).send(err);
-	    }
-
-	    child.exec(`pandoc files/${convertFile.name} --to=markdown`, (error, stdout, stderr) => {
-		if (error) {
-		    return res.status(500).send(err);
-		} else {
-		    converted.markdown = stdout;
-		    return converted;
-		}
-	    });
 	    
-	    return res.json(converted);
+	    console.log(convertFile.name);
+	    
+	    let oldFiles = fs.readdirSync('files/mediatmp/media');
+	    oldFiles.forEach(f => fs.unlinkSync(path.join('files/mediatmp/media/',f)));
+	    
+	    
+	    child.exec(`pandoc "files/${convertFile.name}" --to=markdown --extract-media=files/mediatmp`, (error, stdout, stderr) => {
+	    	if (error) {
+	    	    return res.status(500).send(err);
+	    	} else {	    
+	    	    converted.markdown = stdout;
+		    let newFiles = fs.readdirSync('files/mediatmp/media');
+		    converted.media = newFiles.map(f => path.join('mediatmp/media',f));
+		    return res.send(converted);
+	    	    //return converted;
+	    	}
+	    });
+
+	    
+	    // process.stdout.on('data', function (output) {
+	    // 	converted.markdown = output;
+	    // 	let newFiles = fs.readdirSync('files/mediatmp/media');
+	    // 	console.log(newFiles);
+	    // 	converted.media = newFiles.map(f => path.join('mediatmp/media',f));
+	    // 	return res.send(converted);
+	    // });
+	    
 	    
 	});
     } else {
@@ -53,7 +68,7 @@ app.post('/import', function(req, res) {
     }
 });
 
-
+app.use(express.static('files'));
 
 app.listen(port, function () {
   console.log(`NMH backend app listening on port ${port}!`);
